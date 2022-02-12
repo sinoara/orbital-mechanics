8fa8494ce3c1abb784162087c98f19c3f3996426
Guilherme Sinoara <gsinoara@gmail.com>
2022-02-12 18:52:43 +0100

    added comment to avoid mypy error

--- test/test_physics.py
+++ test/test_physics.py
@@ -5,7 +5,8 @@ import unittest
 
 import numpy as np
 
-from scipy.constants import gravitational_constant as g_const
+from scipy.constants import G # type: ignore[import]
+
 
 
 from orbitalmechanics.body import Body
@@ -18,7 +19,7 @@ class DistanceTest(unittest.TestCase):
             initial_velocity=np.array([0, 0, 0]))
         body2 = Body(mass=1, initial_position=np.array([1, 0, 0]),
             initial_velocity=np.array([0, 0, 0]))
-        self.assertEqual(gravity_force(body1, body2), g_const)
+        self.assertEqual(gravity_force(body1, body2), G)
 
 
     def test_grav_force2(self):
@@ -26,11 +27,11 @@ class DistanceTest(unittest.TestCase):
             initial_velocity=np.array([0, 0, 0]))
         body2 = Body(mass=1, initial_position=np.array([1, 2, 0]),
             initial_velocity=np.array([0, 0, 0]))
-        self.assertEqual(gravity_force(body1, body2), g_const*3/4)
+        self.assertEqual(gravity_force(body1, body2), G*3/4)
 
     def test_grav_force3(self):
         body1 = Body(mass=1, initial_position=np.array([0, 1, 0]),
             initial_velocity=np.array([0, 0, 0]))
         body2 = Body(mass=5, initial_position=np.array([3, 1, 4]),
             initial_velocity=np.array([0, 0, 0]))
-        self.assertEqual(gravity_force(body1, body2), g_const/5)
+        self.assertEqual(gravity_force(body1, body2), G/5)
